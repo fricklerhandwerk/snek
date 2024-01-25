ef94a17b8a98e4635f0fffa73b0d89d5515e30a5
fricklerhandwerk <valentin@fricklerhandwerk.de>
2024-01-25 09:54:03 +0100

    add a second snake: it's now multiplayer!
    - model the snake as a self-contained class
    - factor out the movement logic into a Game method

--- snek/main.py
+++ snek/main.py
@@ -2,6 +2,7 @@
 
 import random
 
+from enum import Enum
 from typing import Any, List, Optional, Set, Tuple
 
 from blessed import Terminal
@@ -12,6 +13,34 @@ from .screen import Buffer, Color, Coordinate, Screen, Shape
 def main():
     Game(Terminal()).run()
 
+class Direction(Enum):
+    Up = (0, -1)
+    Down = (0, 1)
+    Left = (-1, 0)
+    Right = (1, 0)
+
+class Snake:
+    body: List[Coordinate]
+    direction: Direction
+
+    def __init__(self, head: Coordinate, length: int, direction: Direction):
+        x, y = head
+        dx, dy = direction.value
+        self.body = [(x - dx * i, y - dy * i) for i in range(length)]
+
+    def shape(self, color: Color):
+        return Shape(color=color,coordinates=set(self.body))
+
+    @property
+    def head(self):
+        return self.body[0]
+
+    def move(self, direction: Direction, grow: bool = False):
+        hx, hy = self.head
+        dx, dy = direction.value
+        self.body = [(hx + dx, hy + dy)] + self.body
+        if not grow:
+            self.body = self.body[:-1]
 
 class Game():
     def __init__(self, terminal):
@@ -30,9 +59,11 @@ class Game():
         self.restart = False
         # the snake is a sequence of (x,y) coordinates,
         # the first element is the head
-        self.snake = [(3,1),(2,1),(1,1)]
+        self.snake_1 = Snake(head=(3,1), length=3, direction=Direction.Right)
+        self.snake_2 = Snake(head=(self.width-4,self.height-2), length=3, direction=Direction.Left)
         self.obstacles = rectangle(self.width, self.height)
         self.place_apple()
+
 
     def place_apple(self):
         # it's all a lie: the apple is never actually eaten, just moved around...
@@ -51,15 +82,17 @@ class Game():
             # TODO: show welcome screen with key bindings info and speed selection
             while not self.exit:
                 self.reset()
+
+                background = self.screen.buffer()
+                background.contents = [[Color.black for x in y] for y in background.contents]
+
                 while not any((self.restart, self.exit)):
-                    background = self.screen.buffer()
-                    background.contents = [[Color.black for x in y] for y in background.contents]
                     obstacles = Shape(Color.white, self.obstacles)
-                    snake = Shape(Color.green, set(self.snake))
                     apple = Shape(Color.red, set([self.apple]))
                     self.screen.overlay(background)
                     self.screen.draw(obstacles)
-                    self.screen.draw(snake)
+                    self.screen.draw(self.snake_1.shape(Color.green))
+                    self.screen.draw(self.snake_2.shape(Color.pink))
                     self.screen.draw(apple)
                     self.screen.update()
                     self.read_key()
@@ -78,30 +111,42 @@ class Game():
             self.restart = True
             return
 
-        def step(x, y):
-            if (x, y) not in self.occupied_cells:
-                self.snake = [(x, y)] + self.snake
-                if (x, y) != self.apple:
-                    self.snake = self.snake[:-1]
-                else:
-                    self.place_apple()
+        # left player
+        if val == 's':
+            self.move(self.snake_1, Direction.Down)
+        if val == 'w':
+            self.move(self.snake_1, Direction.Up)
+        if val == 'a':
+            self.move(self.snake_1, Direction.Left)
+        if val == 'd':
+            self.move(self.snake_1, Direction.Right)
 
+        # right player
         if val.code == t.KEY_DOWN:
-            x, y = self.snake[0]
-            step(x, y + 1)
+            self.move(self.snake_2, Direction.Down)
         if val.code == t.KEY_UP:
-            x, y = self.snake[0]
-            step(x, y - 1)
+            self.move(self.snake_2, Direction.Up)
         if val.code == t.KEY_LEFT:
-            x, y = self.snake[0]
-            step(x - 1, y)
+            self.move(self.snake_2, Direction.Left)
         if val.code == t.KEY_RIGHT:
-            x, y = self.snake[0]
-            step(x + 1, y)
+            self.move(self.snake_2, Direction.Right)
+
+    def move(self, snake, direction):
+        """
+        Move a snake in the given direction.
+        """
+        hx, hy = snake.head
+        dx, dy = direction.value
+        new_position = (hx + dx, hy + dy)
+        if new_position not in self.occupied_cells:
+            found_apple = new_position == self.apple
+            if found_apple:
+                self.place_apple()
+            snake.move(direction, found_apple)
 
     @property
     def occupied_cells(self):
-        return set(self.snake) | self.obstacles
+        return set(self.snake_1.body) | set(self.snake_2.body) | self.obstacles
 
     @property
     def free_cells(self) -> Set[Coordinate]:
